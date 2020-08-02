4930c12c560b1b93b662558ebee58fd2bf2c7564
BaimukanovRustam <170107097@stu.sdu.edu.kz>
2020-08-02 21:33:39 +0600

    +Logic completed,method is not beatiful(will be changed)

--- src/kz/my/Main.java
+++ src/kz/my/Main.java
@@ -1,48 +1,41 @@
 package kz.my;
 
-import kz.my.classes.Characteristics;
-import kz.my.interfaces.ICharacteristics;
-
 public class Main {
+
 
     public static void main(String[] args) {
 //        Characteristics characteristics = new Characteristics();
 //        System.out.println(characteristics.calculatePower(444));
         int power = 1;
-        int totalScore = 35;
-        int totalPower = 0;
+//        int totalScore = 41;
+        int totalPower = 1;
+        int total = 0;
+        int tempPower = 0;
+        int tempScore = 0;
+        int remainder = 0;
 
-        int x;
-        int c = 0;
+//        System.out.println(calculatePower2());
 
-//        System.out.println(22/10+1);
-//        while (power > totalPower){
-////            x = (score / 10) + 1;
-//            totalPower = totalPower + (score / 10) + 1;
-//            score++;
-//
-////            c++;
-//
-//            System.out.println(totalPower);
-//        }
-//        for (int score = 0; score < 11; score++) {
-//            totalPower = totalPower + (totalPower / 10) + 1;
-//            totalScore -= (totalPower / 10) + 1;
-//            System.out.println(totalPower);
-//
-//        }
-//        System.out.println(totalScore);
-        while (totalScore >= 0){
-            totalPower = totalPower + (totalPower / 10) + 1;
-            totalScore -= (totalPower / 10) + 1;
-            if(totalScore < 0){
+
+    }
+    public static int[] calculatePower2(int totalScore){
+        int totalPower = 1;
+        int spendedScore = 0;
+        int tempPower = 0;
+        int tempScore = 0;
+        int remainder = 0;
+        while (spendedScore < totalScore){
+            tempPower = totalPower;
+            tempScore = spendedScore;
+            totalPower += 1;
+            spendedScore += totalPower / 10 +1;
+            if (spendedScore > totalScore){
+                totalPower = tempPower;
+                spendedScore = tempScore;
                 break;
-
             }
-            power++;
-            System.out.println("Total Score:" + totalScore);
-            System.out.println("Total power:" + totalPower);
-        }
-        System.out.println(power);
+            System.out.println("Power:" + totalPower + " Total Score:" + spendedScore);
+        }remainder = totalScore - spendedScore;
+        return new int[] {remainder, totalPower};
     }
 }
